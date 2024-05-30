1623205a538277e7e67fe8c8c72b4564de6bc83a
Maruf Hossain <mdmarufh974@gmail.com>
2024-05-30 15:53:14 +0600

    new feature adding

---
+++ src/Pages/Home/JobRecommended.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JobRecommended = () => {
+    return (
+        <div>
+            This is new featcher 
+        </div>
+    );
+};
+
+export default JobRecommended;
